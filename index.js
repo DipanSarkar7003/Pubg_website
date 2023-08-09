0404a80f35c66065bf94d7fe94ffd6bb2bfe28c7
Dipan Sarkar <alexmithun1005@gmail.com>
2023-08-09 22:57:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+console.log("hello world")
+let navItem = document.querySelectorAll(".listItem")
+// console.log(navItem)
+
+// navItem.addEventListner("click", function(e){
+//     navItem.classList.add("yellowButton");
+// } )
+
+
+let container2RightBox = document.querySelector(".container2RightBox")
+window.addEventListener("scroll" , function(){
+    container2RightBox.
+    console.log(container2RightBox)
+})
